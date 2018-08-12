1ad2e5f80523d694801d9b18610c8400287ac3e3
Luc Frébourg <l.frebourg@gmail.com>
2018-08-12 19:15:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ ini_set('display_errors', 1);
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
-            ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
+            ->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
